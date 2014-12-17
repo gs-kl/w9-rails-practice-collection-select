bfad86df452dcf26b83ec41c13f5ff75663a658d
Kevin <litchfield.kevin+gs@gmail.com>
2014-12-17 15:33:40 -0800

    ^_^

--- app/models/person.rb
+++ app/models/person.rb
@@ -4,4 +4,8 @@ class Person < ActiveRecord::Base
     "#{first_name} #{last_name}"
   end
 
+  has_many :employments
+  has_many :job_titles, through: :employments
+  has_many :organizations, through: :employments
+  has_many :locations, through: :employments
 end
